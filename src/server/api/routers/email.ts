9a3647ef041de42149e91f0b780cd456749a5b17
Juan Uicich <juan@umamicollective.com>
2025-03-07 20:11:05 +0100

    init email api

--- src/server/api/routers/email.ts
+++ src/server/api/routers/email.ts
@@ -1,39 +1,21 @@
 import { z } from "zod";
-
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
-import { emails } from "~/server/db/schema";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
+import { getUserEmails, getUserEmailsWithFilters } from "~/server/db/queries";
 
 export const emailRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
-  create: protectedProcedure
-    .input(z.object({ subject: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(emails).values({
-        subject: input.subject,
-        ownedById: ctx.session.user.id,
-      });
-    }),
-
-  getLatest: protectedProcedure.query(async ({ ctx }) => {
-    const email = await ctx.db.query.emails.findFirst({
-      orderBy: (emails, { desc }) => [desc(emails.createdAt)],
-    });
-
-    return email ?? null;
+  getAll: protectedProcedure.query(async () => {
+    return getUserEmails();
   }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+  getFiltered: protectedProcedure
+    .input(
+      z.object({
+        starred: z.boolean().optional(),
+        read: z.boolean().optional(),
+        categoryId: z.string().uuid().optional(),
+      })
+    )
+    .query(async ({ input }) => {
+      return getUserEmailsWithFilters(input);
+    }),
 });
