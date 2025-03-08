77ee98344ad891ed322c2ecb78562a7648303fb9
Juan Uicich <juan@umamicollective.com>
2025-03-08 17:58:50 +0100

    remove hand-rolled jwt decode

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,7 +2,8 @@ import "server-only";
 import { NextResponse } from 'next/server'
 // The client you created from the Server-Side Auth instructions
 import { createClient } from '~/lib/supabase/server'
-import { jwtDecode } from "~/lib/utils";
+import { jwtDecode } from "jwt-decode";
+
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
@@ -18,11 +19,13 @@ export async function GET(request: Request) {
       const { data, error } = await supabase.auth.getSession();
       const { session } = data;
       const identities = session?.user.identities || [];
-      const jwt = jwtDecode(session?.access_token);
-      const identity = identities.find(i => i.identity_data.provider_id == jwt.user_metadata.provider_id);
+      if (session?.access_token) {
+        const jwt = jwtDecode(session.access_token);
+        const identity = identities.find(i => i.identity_data?.provider_id == jwt.user_metadata.provider_id);
 
-      await supabase.from('account').update({ refresh_token: session?.provider_refresh_token, access_token: session?.provider_token }).eq('identity_id', identity?.identity_id).select();
+        await supabase.from('account').update({ refresh_token: session?.provider_refresh_token, access_token: session?.provider_token }).eq('identity_id', identity?.identity_id).select();
 
+      }
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development'
       if (isLocalEnv) {
