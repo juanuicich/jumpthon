d0838f364352dd38da88c81dc6f07f5da1e89cbf
Juan Uicich <juan@umamicollective.com>
2025-03-08 14:20:45 +0100

    use supabase types on frontend

---
+++ src/lib/global.d.ts
@@ -0,0 +1,7 @@
+import type { Database as DB } from "~/lib/database.types";
+
+declare global {
+  type Database = DB;
+  type Category = DB["public"]["Tables"]["category"]["Row"];
+  type Email = DB["public"]["Tables"]["email"]["Row"];
+}
